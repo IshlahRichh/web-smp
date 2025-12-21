771461e7a5fa1184e497c830bdbb99a9ea575acd
IshlahRichh <ishlah@ce.student.pens.ac.id>
2025-12-21 18:01:59 +0700

    all in

--- src/layouts/headers/Menu/Offcanvas.tsx
+++ src/layouts/headers/Menu/Offcanvas.tsx
@@ -1,26 +1,11 @@
 import { Link } from "react-router-dom";
 import MobileMenu from "./MobileMenu";
-import { useState } from "react";
-
 interface MobileSidebarProps {
    offCanvas: boolean;
    setOffCanvas: (offCanvas: boolean) => void;
 }
 
 const Offcanvas = ({ offCanvas, setOffCanvas }: MobileSidebarProps) => {
-
-   const [searchValue, setSearchValue] = useState("");
-
-   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchValue(event.target.value);
-   };
-
-   const handleSubmit = (e: React.FormEvent) => {
-      e.preventDefault();
-      setSearchValue('');
-      setOffCanvas(false);
-   };
-
    return (
       <div className={offCanvas ? "mobile-menu-visible" : ""}>
          <div className="tgmobile__menu">
@@ -29,26 +14,14 @@ const Offcanvas = ({ offCanvas, setOffCanvas }: MobileSidebarProps) => {
                <div className="nav-logo">
                   <Link to="/"><img src="/assets/img/logo/logo-blue.png" alt="logo" /></Link>
                </div>
-               <div className="tgmobile__search">
-                  <form onSubmit={handleSubmit}>
-                     <input
-                        type="text"
-                        placeholder="Search here..."
-                        value={searchValue}
-                        onChange={handleSearchChange}
-                     />
-                  </form>
-               </div>
                <div className="tgmobile__menu-outer">
                   <MobileMenu />
                </div>
-               <div className="social-links">
-                  <ul className="list-wrap">
-                     <li><Link to="https://www.instagram.com/spemduta"><i className="fab fa-instagram"></i></Link></li>
-                     <li><Link to="https://www.tiktok.com/@spemduta"><i className="fab fa-tiktok"></i></Link></li>
-                     <li><Link to="https://www.youtube.com/@spemduta135"><i className="fab fa-youtube"></i></Link></li>
-                     <li><Link to="https://www.facebook.com/spemduta"><i className="fab fa-facebook-f"></i></Link></li>
-                  </ul>
+               <div className="offCanvas__social-icon mt-30 text-center">
+                     <Link to="https://www.instagram.com/spemduta"><i className="fab fa-instagram"></i></Link>
+                     <Link to="https://www.tiktok.com/@spemduta"><i className="fab fa-tiktok"></i></Link>
+                     <Link to="https://www.youtube.com/@spemduta135"><i className="fab fa-youtube"></i></Link>
+                     <Link to="https://www.facebook.com/spemduta"><i className="fab fa-facebook-f"></i></Link>
                </div>
             </nav>
          </div>
